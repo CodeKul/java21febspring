cea0f817b57c26053d67b3179671f5bae6984569
Vaibhav N <vn202207@nexttechvision.com>
2023-05-30 16:02:13 +0530

    jpa methods

--- src/main/java/com/codekul/java21febspring/jpa/StudentController.java
+++ src/main/java/com/codekul/java21febspring/jpa/StudentController.java
@@ -3,7 +3,9 @@ package com.codekul.java21febspring.jpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -17,12 +19,13 @@ public class StudentController {
         studentRepository.save(student);
         return "student saved.";
     }
+
     @PutMapping("update")
     public String updateStudent(@RequestBody Student student) {
-         Student st = studentRepository.getReferenceById(student.getId());
-         st.setAddress(student.getAddress());
-         st.setName(student.getName());
-         studentRepository.save(st);
+        Student st = studentRepository.getReferenceById(student.getId());
+        st.setAddress(student.getAddress());
+        st.setName(student.getName());
+        studentRepository.save(st);
 
         return "student updated";
     }
@@ -42,22 +45,57 @@ public class StudentController {
         return studentRepository.findById(id);
     }
 
-     @DeleteMapping("delete/{id}")
+    @DeleteMapping("delete/{id}")
     public String deleteStudent(@PathVariable("id") Long id) {
-         studentRepository.deleteById(id);
-         return "record deleted..";
-    }
-     @DeleteMapping("delete1/{id}")
-    public String deleteStudent1(@PathVariable("id") Long id) {
-         Optional<Student> student = studentRepository.findById(id);
-         studentRepository.delete(student.get());
-         return "record deleted..";
+        studentRepository.deleteById(id);
+        return "record deleted..";
     }
 
-     @DeleteMapping("delete2/{id}")
+    @DeleteMapping("delete1/{id}")
+    public String deleteStudent1(@PathVariable("id") Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        studentRepository.delete(student.get());
+        return "record deleted..";
+    }
+
+    @DeleteMapping("delete2/{id}")
     public String deleteStudent2() {
-         studentRepository.deleteAll();
-         return "all record deleted..";
+        studentRepository.deleteAll();
+        return "all record deleted..";
+    }
+
+    @DeleteMapping("deleteAll/{ids}")
+    public String deleteStudent2(@PathVariable("ids") List<Long> ids) {
+        studentRepository.deleteAllById(ids);
+        return "all record deleted..";
+    }
+
+    @GetMapping("getStudentByName/{name}")
+    public Map<String, Object> deleteStudent2(@PathVariable("name") String name) {
+        Student student = studentRepository.findByName(name);
+        Map<String, Object> map = new HashMap<>();
+        if (student != null) {
+            map.put("message", "record found");
+            map.put("stud", student);
+        } else {
+            map.put("message", "record not found");
+
+        }
+        return map;
+    }
+  @GetMapping("getStudentByNameAndAddress/{name}/{address}")
+    public Map<String, Object> getStudentByName1(@PathVariable("name") String name,
+                                                 @PathVariable("address") String address) {
+        List<Student> student = studentRepository.findByNameAndAddress(name,address);
+        Map<String, Object> map = new HashMap<>();
+        if (student != null) {
+            map.put("message", "record found");
+            map.put("stud", student);
+        } else {
+            map.put("message", "record not found");
+
+        }
+        return map;
     }
 
 
--- src/main/java/com/codekul/java21febspring/jpa/StudentRepository.java
+++ src/main/java/com/codekul/java21febspring/jpa/StudentRepository.java
@@ -3,6 +3,11 @@ package com.codekul.java21febspring.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    Student findByName(String name);
+
+    List<Student> findByNameAndAddress(String name, String address);
 }
