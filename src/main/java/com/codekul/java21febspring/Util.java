8b0a7144a61cac120ece398897058215c8486ef2
Vaibhav N <vn202207@nexttechvision.com>
2023-07-03 15:54:15 +0530

    mail sender

---
+++ src/main/java/com/codekul/java21febspring/Util.java
@@ -0,0 +1,8 @@
+package com.codekul.java21febspring;
+
+public class Util {
+    public static String USER_NAME = "codekulvaibhav94@gmail.com";  // GMail user name (just the part before "@gmail.com")
+    public static String PASSWORD = "vaibhav6161"; // GMail password
+
+    public static String RECIPIENT = "bandalricha99@gmail.com";
+}
--- src/main/java/com/codekul/java21febspring/aop/UsersController.java
+++ src/main/java/com/codekul/java21febspring/aop/UsersController.java
@@ -1,6 +1,8 @@
 package com.codekul.java21febspring.aop;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,40 @@ public class UsersController {
     @Autowired
     private UsersRepository usersRepository;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     @PostMapping("saveUser")
     public String saveUser(@RequestBody Users users) {
         usersRepository.save(users);
+//        https://myaccount.google.com/u/0/apppasswords
+        sendMail();
         return "user saved.";
     }
+
 
     @PostMapping("login")
     public String login(@RequestBody LoginRequestDto loginRequestDto) {
         String userNm = usersRepository.getLoginUser(loginRequestDto.getUserName(), loginRequestDto.getPassword());
-        if(userNm != null){
+        if (userNm != null) {
             return "login successfully..";
-        }else {
+        } else {
             return "login failed";
         }
+    }
+
+    private void sendMail() {
+        String from = "codekul.vaibhav@gmail.com";
+        String to = "bandalricha99@gmail.com";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject("This is a plain text email");
+        message.setText("Hello guys! This is a plain text email.");
+
+        mailSender.send(message);
     }
 }
 /*
